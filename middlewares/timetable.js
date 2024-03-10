4d10cdc9bee0be1f910abb86a5e249c87596d9ff
Xurshidbek <xurshidbekabdulakimov@gmail.com>
2024-03-10 20:18:35 +0500

    Update puppeteer launch arguments

--- middlewares/timetable.js
+++ middlewares/timetable.js
@@ -13,7 +13,7 @@ async function timetable({ className }) {
         // lounch browser
         browser = await puppeteer.launch({
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu', '--no-first-run', '--no-zygote', '--single-process', '--disable-extensions', '--ignore-certificate-errors', '--ignore-certificate-errors-spki-list', '--enable-features=NetworkService', '--disable-features=NetworkService']
         });
         const page = await browser.newPage();
 
