3928a2c86ce7623a377720345a5b6ffc0b554181
Xurshidbek <69305667+abdulakimov@users.noreply.github.com>
2024-09-11 23:41:12 +0500

    Update main.js

--- main.js
+++ main.js
@@ -9,16 +9,6 @@ config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-}
-);
-
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const classNameScene = new Scenes.BaseScene("classNameScene");
