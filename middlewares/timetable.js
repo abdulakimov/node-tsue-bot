9f9b4d604201eae312dca877dfffe069abdcecd5
Xurshidbek <xurshidbekabdulakimov@gmail.com>
2025-01-20 19:23:30 +0500

    Finishing

--- middlewares/timetable.js
+++ middlewares/timetable.js
@@ -1,6 +1,5 @@
-// import puppeteer from "puppeteer";
+import puppeteer from "puppeteer";
 import select from "puppeteer-select";
-const chromium = require('chrome-aws-lambda');
 
 export async function timetableForStudent({ className }) {
     let browser;
@@ -11,17 +10,14 @@ export async function timetableForStudent({ className }) {
     }
 
     try {
-        // launch browser
-        // browser = await puppeteer.launch({
-        //     headless: true,
-        //     args: ['--no-sandbox', '--disable-setuid-sandbox']
-        // });
 
-        browser = await chromium.puppeteer.launch({
-            args: chromium.args,
-            executablePath: await chromium.executablePath,
-            headless: chromium.headless,
+
+        browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
+
+
 
         const page = await browser.newPage();
 
